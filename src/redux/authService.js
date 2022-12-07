34e7e2df73b9d04d95d2eeeae07f1e44b66dc041
Kalyan-Saxena <kalyansaxena123@gmail.com>
2022-12-07 23:03:19 +0530

    update api url

--- src/redux/authService.js
+++ src/redux/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://mern-authentication-backend.onrender.com/api/auth";
 
 // Register User
 const register = async (userData) => {
